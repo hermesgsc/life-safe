4b8a5bd07d5bfa0391a0e71d6a055b4045647170
Hermes Gabriel <programadorgabriel22@gmail.com>
2024-05-25 21:05:57 -0300

    erro em identacao corrijido

--- src/Components/We-care/index.jsx
+++ src/Components/We-care/index.jsx
@@ -1,22 +1,25 @@
-import "./wecare.css";
+import React from "react";
+import "./wecare.css"; // Importe seu arquivo CSS aqui
 
 const WeCare = () => {
   return (
-    <section className="container-wecare">
-      <img src="./images/wecare.png" alt="" />
-
-      <div className="overlay-title">
-        <h3>Sua segurança é o que importa</h3>
-      </div>
-      <div className="overlay-text">
-        <p>+ de 1 milhão de clientes atendidos</p>
-        <p>50 anos de experiência</p>
-        <p>Franquias em todos os estados do Brasil</p>
-        <p>Suporte 24 horas disponível</p>
-      </div>
-      <div className="image-logo2">
-          <img src="./images/mobilelogo2.png" alt="" />
+    <section className="secao-com-imagem">
+      <div className="conteudo">
+        <div className="texto">
+          <h1>Nós nos preocupamos com a segurança da sua família</h1>
+          <p>+ de 1 milhão de clientes atendidos</p>
+          <p>50 anos de ajudando milhares de famílias</p>
+          <p>Franquias em todos os estados do Brasil</p>
+          <p>Suporte 24 horas disponível</p>
         </div>
+        <div className="logotipo-container">
+        <img
+            src="./images/mobilelogo2.png"
+            alt="Seu Logotipo"
+            className="logotipo"
+          />
+          </div>
+      </div>
     </section>
   );
 };
