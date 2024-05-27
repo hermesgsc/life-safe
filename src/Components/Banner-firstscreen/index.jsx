6d9c0fb9c3f9ed642b20faf798d23cf58346ea6a
Hermes Gabriel <programadorgabriel22@gmail.com>
2024-05-27 09:07:09 -0300

    efeito smooth adicionado ao clicar no botao

--- src/Components/Banner-firstscreen/index.jsx
+++ src/Components/Banner-firstscreen/index.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import "./firstscreen.css";
 
 const Banner = () => {
+  const handleClick = (e) => {
+    e.preventDefault();
+    const targetId = e.currentTarget.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <section className="container-banner">
       <div className="banner-logo">
         <img className="logo background" src="./images/mobile1.jpg" alt="Mobile" />
         <img className="logo overlay" src="./images/mobilelogo1.png" alt="Mobile Logo" />
-        <a href='#Lead' className="banner-button">Contato</a>
+        <a href="#Lead" className="banner-button" onClick={handleClick}>Contato</a>
       </div>
     </section>
   );
